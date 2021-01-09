86f07b1956dce4abf38cb88048e9384a967e3a29
Panishdudda <45222682+Panishdudda@users.noreply.github.com>
2021-01-09 12:06:34 +0530

    Update start.py

--- signal_package/start.py
+++ signal_package/start.py
@@ -14,33 +14,26 @@ def start(self,):
 
     while(True):
 
-        #object for machine on/off signal
+           #object for machine on/off signal
            getCurrentSignal(self,machineSignalInputPin,"machineON","machineOFF")
 
            #object for cycle on/off signal
            getCurrentSignal(self,cycleSignalInputPin,"cycleON","cycleOFF")
 
-
            #object for m30 on/off signal
            getCurrentSignal(self,m30SignalInputPin,"m30ON","m30OFF")
 
-
-            #object for alarm on/off signal
+           #object for alarm on/off signal
            getCurrentSignal(self,alarmSignalInputPin,"alarmON","alarmOFF")
 
-
-            #object for emergency on/off signal
+           #object for emergency on/off signal
            getCurrentSignal(self,emergencySignalInputPin,"emergencyON","emergencyOFF")
-
-
 
            #object for spindle on/off signal
            getCurrentSignal(self,spindleSignalInputPin,"spindleON","spindleOFF")
 
            #object for reset on/off signal
            getCurrentSignal(self,resetSignalInputPin,"resetON","resetOFF")
-
-
           
            #object for runOutNotOk on/off signal
            getCurrentSignal(self,runOutNotOkSignalInputPin,"runoutNotOkON","runoutNotOkOFF")
